dbfdaa86be68d35ee9a09746b5744c1eb61e266c
Huzajfha Bayoumi <hubay22@student.sdu.dk>
2025-04-28 17:03:29 +0200

    some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
+import React, {  } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import UpcomingMovies from "./UpcomingMovies";
 import UpcomingSnapshots from "./UpcomingSnapshots";
 import NowPlayingMovies from "./NowPlayingMovies";
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
+
 
     return (
         <Router>
